c0d89a8977b1d4b36176da2da1841aa67e06c12e
Eric <132069567+EricXiaomaimai@users.noreply.github.com>
2023-05-20 16:58:40 +0800

    Create ASCII_addition.c

---
+++ math/ASCII/ASCII_addition.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define ASCII_A 65
+#define ASCII_Z 90
+
+int main(void)
+{
+    char c = 'A';
+    int ascii_n = 65;
+
+    while (c >= ASCII_A && c <= ASCII_Z)
+    {
+        printf("%c + 1=%c", c, c + 1);
+        putchar('!');
+        printf("\n");
+
+        c++;
+    }
+    
+    return 0;
+}
